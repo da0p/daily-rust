663dbe06a286b6d99ed7726f881ae42a83257797
Phuong Dao <phuong018@gmail.com>
2023-11-07 20:45:31 +0100

    [Daily-12] Chapter-20: Multi-threaded http server

---
+++ trpl/chapter-20/multi_threaded_server/src/main.rs
@@ -0,0 +1,40 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+use multi_threaded_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "200.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    // we call as_bytes() to convert string data to bytes. write_all method takes
+    // a &[u8] and sends those bytes directly down the connection.
+    stream.write_all(response.as_bytes()).unwrap();
+}
