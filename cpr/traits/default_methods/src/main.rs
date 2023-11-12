5531b4e180fcdb764adf9d3fb6f73bd2c883b769
Phuong Dao <phuong018@gmail.com>
2023-11-12 21:38:45 +0100

    [Daily-15] Default methods for trait

---
+++ cpr/traits/default_methods/src/main.rs
@@ -0,0 +1,23 @@
+trait Equals {
+    fn equals(&self, other: &Self) -> bool;
+    // a trait can be implemented in terms of other trait methods
+    fn not_equals(&self, other: &Self) -> bool {
+        !self.equals(other)
+    }
+}
+
+#[derive(Debug)]
+struct Centimeter(i16);
+
+impl Equals for Centimeter {
+    fn equals(&self, other: &Centimeter) -> bool {
+        self.0 == other.0
+    }
+}
+
+fn main() {
+    let a = Centimeter(10);
+    let b = Centimeter(20);
+    println!("{a:?} equals {b:?}: {}", a.equals(&b));
+    println!("{a:?} not_equals {b:?}: {}", a.not_equals(&b));
+}
