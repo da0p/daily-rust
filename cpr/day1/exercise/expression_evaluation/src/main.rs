e5c409b4575783bbd7e4e78090f3074198f41a62
Phuong Dao <phuong018@gmail.com>
2023-11-14 22:07:14 +0100

    [Daily-17] Expression evaluation

---
+++ cpr/day1/exercise/expression_evaluation/src/main.rs
@@ -0,0 +1,123 @@
+/// An operation to perform on two subexpressions.
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+/// An expression, in tree form.
+#[derive(Debug)]
+enum Expression {
+    /// An operation on two subexpressions.
+    Op {
+        op: Operation,
+        left: Box<Expression>,
+        right: Box<Expression>,
+    },
+
+    /// A literal value
+    Value(i64),
+}
+
+/// The result of evaluating an expression.
+#[derive(Debug, PartialEq, Eq)]
+enum Res {
+    /// Evaluation was successful, with the given result.
+    Ok(i64),
+    /// Evaluation failed, with the given error message.
+    Err(String),
+}
+// Allow `Ok` and `Err` as shorthands for `Res::Ok` and `Res::Err`.
+use Res::{Err, Ok};
+use crate::Expression::{Value, Op};
+
+fn eval(e: Expression) -> Res {
+    match e {
+        Value(val) => Ok(val),
+        Op{op, left, right} => {
+            let left = match eval(*left) {
+                Ok(lhs) => lhs,
+                Err(err) => return Err(err),
+            };
+
+            let right = match eval(*right) {
+                Ok(rhs) => rhs,
+                Err(err) => return Err(err),
+            };
+
+            let result = match op {
+                Operation::Add => Ok(left + right),
+                Operation::Sub => Ok(left - right),
+                Operation::Mul => Ok(left * right),
+                Operation::Div => {
+                    if right != 0 {
+                       Ok(left / right)
+                    } else {
+                        Err(String::from("division by zero"))
+                    }
+                }
+            };
+
+            result
+        }
+    }
+}
+
+#[test]
+fn test_value() {
+    assert_eq!(eval(Expression::Value(19)), Ok(19));
+}
+
+#[test]
+fn test_sum() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(10)),
+            right: Box::new(Expression::Value(20)),
+        }),
+        Ok(30)
+    );
+}
+
+#[test]
+fn test_recursion() {
+    let term1 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Value(10)),
+        right: Box::new(Expression::Value(9)),
+    };
+    let term2 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(3)),
+            right: Box::new(Expression::Value(4)),
+        }),
+        right: Box::new(Expression::Value(5)),
+    };
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(term1),
+            right: Box::new(term2),
+        }),
+        Ok(85)
+    );
+}
+
+#[test]
+fn test_error() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Div,
+            left: Box::new(Expression::Value(99)),
+            right: Box::new(Expression::Value(0)),
+        }),
+        Err(String::from("division by zero"))
+    );
+}
+
+fn main() {}
