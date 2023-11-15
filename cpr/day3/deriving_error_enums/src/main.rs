a876ad9cc8037e9670c2740d216c60d622d204da
Phuong Dao <phuong018@gmail.com>
2023-11-15 17:52:34 +0100

    [Daily-18] Error Handling: Deriving error enums

---
+++ cpr/day3/deriving_error_enums/src/main.rs
@@ -0,0 +1,27 @@
+use std::{fs, io};
+use std::io::Read;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum ReadUsernameError {
+    #[error("Could not read: {0}")]
+    IoError(#[from] io::Error),
+    #[error("Found no username in {0}")]
+    EmptyUsername(String),
+}
+
+fn read_username(path: &str) -> Result<String, ReadUsernameError> {
+    let mut username = String::new();
+    fs::File::open(path)?.read_to_string(&mut username)?;
+    if username.is_empty() {
+        return Err(ReadUsernameError::EmptyUsername(String::from(path)));
+    }
+    Ok(username)
+}
+
+fn main() {
+    match read_username("config.dat") {
+        Ok(username) => println!("Username: {username}"),
+        Err(err) => println!("Error: {err}"),
+    }
+}
