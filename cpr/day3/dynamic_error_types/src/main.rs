40475eb27afb1b6a6d165e9fa26b1d99cf216fb3
Phuong Dao <phuong018@gmail.com>
2023-11-15 18:03:35 +0100

    [Daily-18] Error Handling: Dynamic error types

---
+++ cpr/day3/dynamic_error_types/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::io::Read;
+use thiserror::Error;
+use std::error::Error;
+
+#[derive(Clone, Debug, Eq, Error, PartialEq)]
+#[error("Found no username in {0}")]
+struct EmptyUsernameError(String);
+
+fn read_username(path: &str) -> Result<String, Box<dyn Error>> {
+    let mut username = String::new();
+    fs::File::open(path)?.read_to_string(&mut username)?;
+    if username.is_empty() {
+        return Err(EmptyUsernameError(String::from(path)).into());
+    }
+    Ok(username)
+}
+
+fn main() {
+    match read_username("config.dat") {
+        Ok(username) => println!("Username: {username}"),
+        Err(err) => println!("Error: {err}"),
+    }
+}
